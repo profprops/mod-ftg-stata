c124a1ca0a26c5ffe392f0e7eac52290bd804264
The M <76592330+profprops@users.noreply.github.com>
2021-01-15 12:15:05 +0300

    Add files via upload

---
+++ TestMod.py
@@ -0,0 +1,36 @@
+from .. import loader
+from asyncio import sleep
+
+@loader.tds
+class TestMod(loader.Module):
+	strings = {"name":"Test"}
+	@loader.owner
+	async def testcmd(self, message):
+		await message.edit("M")
+		await sleep(1)
+		await message.edit("Mo")
+		await sleep(1)
+		await message.edit("Mod")
+		await sleep(1)
+		await message.edit("Modu")
+		await sleep(1)
+		await message.edit("Modul")
+		await sleep(1)
+		await message.edit("Module")
+		await sleep(1)
+		await message.edit("Module ")
+		await.sleep(1)
+		await message.edit("Module b")
+		await sleep(1)
+		await message.edit("Module by")
+		await sleep(1)
+		await message.edit("Module by ")
+		await sleep(1)
+		await message.edit("Module by M")
+		await sleep(1)
+		await message.edit("Module by MA")
+		await sleep(1)
+		await message.edit("Module by MAZ")
+		await sleep(1)
+		await message.edit("Module by MAZA")
+	
